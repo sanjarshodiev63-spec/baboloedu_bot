5405e83fbb41b2243d93a7c12b61325f623d0998
sanjarshodiev63-spec <sanjarshodiev63@gmail.com>
2026-05-27 10:10:57 +0300

    Update babolo_bot.py

--- babolo_bot.py
+++ babolo_bot.py
@@ -337,7 +337,8 @@ async def tolov_malumotlari(update: Update, context: ContextTypes.DEFAULT_TYPE):
               bank=BANK,
               tel=TELEFON)
     inline = InlineKeyboardMarkup([
-        [InlineKeyboardButton(t("btn_channel", lang), url="tg://join?invite=YlLgTaf9MIwyYmE6")],
+        [InlineKeyboardButton(t("btn_paid", lang), url="https://t.me/mahmudov_mentor")],
+        [InlineKeyboardButton(t("btn_channel", lang), url=https://t.me/+YlLgTaf9MIwyYmE6)],
         [InlineKeyboardButton("💬 WhatsApp", url="https://chat.whatsapp.com/FedvR6riKUh5UQCujxzSLB")]
     ])
     await update.message.reply_text(xabar, parse_mode="HTML", reply_markup=inline)
