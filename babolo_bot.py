df9125acdc1fcdadfe3f45288aa3e045b740929f
sanjarshodiev63-spec <sanjarshodiev63@gmail.com>
2026-05-27 11:16:29 +0300

    Update babolo_bot.py

--- babolo_bot.py
+++ babolo_bot.py
@@ -414,9 +414,9 @@ async def fan_olish(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parse_mode="HTML",
         reply_markup=bosh_menyu(lang)
     )
-    # Admin ga xabar yuborish
-    # ADMIN_CHAT_ID = 123456789
-    # await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=f"🆕 Yangi ariza!\n👤 {ism}\n📱 {telefon}\n📚 {fan}")
+     Admin ga xabar yuborish
+     ADMIN_CHAT_ID = 7821601256
+     await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=f"🆕 Yangi ariza!\n👤 {ism}\n📱 {telefon}\n📚 {fan}")
     return ConversationHandler.END
 
 async def bekor_qilish(update: Update, context: ContextTypes.DEFAULT_TYPE):
